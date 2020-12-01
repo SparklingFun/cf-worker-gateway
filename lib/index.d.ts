619f6a22f4048962ae9ddfd4d8a649936fa2a9a0
SparklingFun <sparkinglemon@foxmail.com>
2020-12-01 07:59:29 +0000

    test: add basic test way, waiting for more rules

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -13,9 +13,13 @@ interface GatewayOption {
     rewrites?: Array<GatewayRewrite>;
     redirects?: Array<GatewayRedirect>;
 }
+interface GatewayHelpers {
+    Request?: Request;
+    Response?: Response;
+}
 interface FetchEvent extends Event {
     request: Request;
     respondWith(response: Promise<Response> | Response): Promise<Response>;
 }
-export default function gateway(event: FetchEvent, options: GatewayOption): Event | Response;
+export default function gateway(event: FetchEvent, options: GatewayOption, helpers?: GatewayHelpers): any;
 export {};
---
+++ test/index.js
@@ -0,0 +1,25 @@
+const Cloudworker = require("@dollarshaveclub/cloudworker");
+const gateway = require("../lib/index").default;
+
+const simpleScript = `
+addEventListener('fetch', event => {
+  let _newEvt = gateway(event, {
+    redirects: [{source: '/test2', destination: '/test-slug'}]
+  }, {
+    Response
+  });
+  event.respondWith(_newEvt instanceof Response ? _newEvt : new Response('hello', {status: 200}));
+})
+`
+const req = new Cloudworker.Request('http://127.0.0.1/test2')
+const cw = new Cloudworker(simpleScript, {
+  bindings: {
+    gateway
+  }
+})
+cw.dispatch(req).then((res) => {
+  console.log("Response Status: ", res.status)
+  res.text().then((body) =>{
+    console.log("Response Body: ", body)
+  })
+})
