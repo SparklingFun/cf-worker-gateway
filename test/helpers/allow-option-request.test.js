222f92cec4a8d0ef9710b65ef9376fafd316ac68
SparklingFun <sparkinglemon@foxmail.com>
2021-01-15 11:37:40 +0000

    fix(test): adapt new test form

--- test/helpers/allow-option-request.test.js
+++ test/helpers/allow-option-request.test.js
@@ -1,21 +1,21 @@
 const bootstrap = require("../bootstrap");
 
-test('[Helpers] Allow OPTIONS request', () => {
-    return bootstrap("/test2", "allowOption()", {
+test('[Helpers] Allow OPTIONS request', async () => {
+    const tester = bootstrap("/test2", "allowOption()", {
         method: 'OPTIONS'
-    }).then(res => {
-        expect(res.status).toBe(204);
     })
+    let res = await tester.run();
+    return expect(res.status).toBe(204);
 });
 
-test('[Helpers] Allow OPTIONS request with custom headers', () => {
-    return bootstrap("/test2", "allowOption()", {
+test('[Helpers] Allow OPTIONS request with custom headers', async () => {
+    const tester = bootstrap("/test2", "allowOption()", {
         method: 'OPTIONS',
         headers: {
             "Test-Header": "this-is-a-test-header",
             "access-control-request-headers": "Test-Header"
         }
-    }).then(res => {
-        expect(res.headers.get("Access-Control-Allow-Headers")).toBe("Test-Header");
     })
+    let res = await tester.run();
+    return expect(res.headers.get("Access-Control-Allow-Headers")).toBe("Test-Header");
 });
--- test/middlewares/rewrite.test.js
+++ test/middlewares/rewrite.test.js
@@ -1,73 +1,73 @@
 const bootstrap = require("../bootstrap");
 const origin = 'http://127.0.0.1';
 
-test('From-to rewrite', () => {
-    return bootstrap('/test2', `rewrite({
+test('From-to rewrite', async () => {
+    const tester = bootstrap('/test2', `rewrite({
         rules: [
             {
                 source: '/test2',
                 destination: '/test2',
             }
         ]
-    })`).then(async res => {
-        const resText = await res.text();
-        expect(resText).toBe(origin + '/test2');
-    })
+    })`)
+    let res = await tester.run();
+    const resText = await res.text();
+    return expect(resText).toBe(origin + '/test2');
 });
-test('Simple Redirect', () => {
-    return bootstrap('/test2', `rewrite({
+test('Simple Redirect', async () => {
+    const tester = bootstrap('/test2', `rewrite({
         rules: [
             {
                 source: '/test2',
                 destination: '/docs/test2',
             }
         ]
-    })`).then(async res => {
-        const resText = await res.text();
-        expect(resText).toBe(origin + '/docs/test2');
-    })
+    })`)
+    const res = await tester.run();
+    const resText = await res.text();
+    return expect(resText).toBe(origin + '/docs/test2');
 });
-test('From-Prefix rewrite', () => {
-    return bootstrap('/test2', `rewrite({
+test('From-Prefix rewrite', async () => {
+    const tester = bootstrap('/test2', `rewrite({
         rules: [
             {
                 source: '/test2',
                 destination: '/docs/test2',
             }
         ]
-    })`).then(async res => {
-        const resText = await res.text();
-        expect(resText).toBe(origin + '/docs/test2');
-    })
+    })`)
+    const res = await tester.run()
+    const resText = await res.text();
+    return expect(resText).toBe(origin + '/docs/test2');
 });
-test('Remove prefix rewrite', () => {
-    return bootstrap('/docs/test2', `rewrite({
+test('Remove prefix rewrite', async () => {
+    const tester = bootstrap('/docs/test2', `rewrite({
         rules: [
             {
                 source: '/docs/*',
                 destination: '/test2',
             }
         ]
-    })`).then(async res => {
-        const resText = await res.text();
-        expect(resText).toBe(origin + '/test2');
-    })
+    })`)
+    const res = await tester.run();
+    const resText = await res.text();
+    return expect(resText).toBe(origin + '/test2');
 });
-test('Remove prefixed deep path rewrite', () => {
-    return bootstrap('/docs/test1/test2/test3', `rewrite({
+test('Remove prefixed deep path rewrite', async () => {
+    const tester = bootstrap('/docs/test1/test2/test3', `rewrite({
         rules: [
             {
                 source: '/docs/**/*',
                 destination: '/test2',
             }
         ]
-    })`).then(async res => {
-        const resText = await res.text();
-        expect(resText).toBe(origin + '/test2');
-    })
+    })`)
+    const res = await tester.run();
+    const resText = await res.text();
+    return expect(resText).toBe(origin + '/test2');
 });
-test('Rewrite with basePath', () => {
-    return bootstrap('/docs/test1/test2/test3', `rewrite({
+test('Rewrite with basePath', async () => {
+    const tester = bootstrap('/docs/test1/test2/test3', `rewrite({
         basePath: '/docs',
         rules: [
             {
@@ -75,8 +75,8 @@ test('Rewrite with basePath', () => {
                 destination: '/test2',
             }
         ]
-    })`).then(async res => {
-        const resText = await res.text();
-        expect(resText).toBe(origin + '/docs/test2');
-    })
+    })`)
+    const res = await tester.run();
+    const resText = await res.text();
+    return expect(resText).toBe(origin + '/docs/test2');
 })
