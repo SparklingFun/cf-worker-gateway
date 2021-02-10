5ada3d966a36026d7bcb4f370a3521523dc2d93e
SparklingFun <sparkinglemon@foxmail.com>
2021-02-10 06:43:42 +0000

    fix(interceptor): fix mismatch bug

--- src/middlewares/interceptor.ts
+++ src/middlewares/interceptor.ts
@@ -6,7 +6,7 @@ export default function Interceptor(path: string, fn: Function) {
     return async function(event: CustomFetchEvent) {
         if(!fn) return event;
         const requestUrl = event.request.url;
-        if(_matchPath(path, requestUrl)) {
+        if(_matchPath(requestUrl, path)) {
             const result = await fn(event);
             if(result instanceof Response || result instanceof FetchEvent) {
                 return result;
