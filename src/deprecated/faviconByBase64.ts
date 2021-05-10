79d1e1885835b9441e2e8a286cc89d20f64b5c9f
arctome <sparkinglemon@foxmail.com>
2021-05-10 04:03:18 +0000

    refactor(deprecated): update favicon

--- src/deprecated/faviconByBase64.ts
+++ src/deprecated/faviconByBase64.ts
@@ -1,22 +1,19 @@
 // helper option faviconBase64 (NOT RECOMMAND)
 export default function faviconByBase64(b64str: string) {
-    return function (event: FetchEvent) {
-        if (event.request.method === 'GET' && event.request.url.endsWith('/favicon.ico')) {
-            if (b64str) {
-                // you can ignore all request of `favicon.ico`
-                if (b64str === 'ignored') {
-                    return new Response(undefined, {
-                        status: 204
-                    })
-                }
-                const buffer = Buffer.from(b64str, 'base64');
-                return new Response(buffer, {
-                    headers: {
-                        'Content-Type': 'image/x-icon'
-                    }
-                })
-            }
-        }
-        return;
+  return function (event: FetchEvent) {
+    if (b64str) {
+      // you can ignore all request of `favicon.ico`
+      if (b64str === "ignored") {
+        return new Response(undefined, {
+          status: 204,
+        });
+      }
+      const buffer = Buffer.from(b64str, "base64");
+      return new Response(buffer, {
+        headers: {
+          "Content-Type": "image/x-icon",
+        },
+      });
     }
+  };
 }
