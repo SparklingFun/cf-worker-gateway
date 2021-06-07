512f6b30bb464d398adfda44e9a47fbfe8377ae9
arctome <sparkinglemon@foxmail.com>
2021-06-07 05:21:19 +0000

    build: 1.0.0-canary.3

--- lib/deprecated/faviconByBase64.d.ts
+++ lib/deprecated/faviconByBase64.d.ts
@@ -1,1 +1,1 @@
-export default function faviconByBase64(b64str: string): (event: FetchEvent) => Response | undefined;
+export default function faviconByBase64(b64str: string, useExactPath?: boolean): (event: FetchEvent) => Response | undefined;
--- lib/helpers/basicAuth.d.ts
+++ lib/helpers/basicAuth.d.ts
@@ -1,6 +1,7 @@
 interface basicAuthOption {
     USER_NAME?: string;
     USER_PASS?: string;
+    realm?: string;
 }
 export default function basicAuth(options: basicAuthOption): (event: FetchEvent) => Response | undefined;
 export {};
