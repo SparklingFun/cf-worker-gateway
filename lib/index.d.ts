97ffc697135a17ca73176aee6ecd25f144448042
arctome <hi@arcto.me>
2021-06-15 06:09:29 +0000

    build: prepare for neew version

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,9 +4,11 @@ import faviconByBase64 from "./deprecated/faviconByBase64";
 import cors from "./helpers/cors";
 import robotsTxt from "./helpers/robotsTxt";
 import basicAuth from "./helpers/basicAuth";
+import { Match } from "path-to-regexp";
 interface FetchEvent extends Event {
     request: Request;
     respondWith(response: Promise<Response> | Response): Promise<Response>;
+    match?: Match;
 }
 declare const WorkerScaffold: (event: FetchEvent, isDev?: boolean) => Function;
 export { redirect, rewrite, faviconByBase64, cors, robotsTxt, basicAuth };
