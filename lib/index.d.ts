65ff5bdb1edbff122a8944a09fc64d3fbe7e7b5b
Wenbo <sparkinglemon@foxmail.com>
2021-06-21 14:22:44 +0800

    v1.0.5-canary.0 (#23)
    * feat(style & extension): add `errorHandler` extension & refact core to class expression
    
    * feat(lint): format & convert new `errorHandler` to static method
    
    * fix(helpers): fix basicAuth did not read default config

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,15 +1,34 @@
+import { Match } from "path-to-regexp";
 import redirect from "./middlewares/redirect";
 import rewrite from "./middlewares/rewrite";
 import faviconByBase64 from "./deprecated/faviconByBase64";
 import cors from "./helpers/cors";
 import robotsTxt from "./helpers/robotsTxt";
 import basicAuth from "./helpers/basicAuth";
-import { Match } from "path-to-regexp";
 interface FetchEvent extends Event {
     request: Request;
     respondWith(response: Promise<Response> | Response): Promise<Response>;
     match?: Match;
 }
-declare const WorkerScaffold: (event: FetchEvent, isDev?: boolean) => Function;
+interface MiddlewareHandlerBundle {
+    default: Function | Promise<Function>;
+    callback?: Function | Promise<Function>;
+}
+declare class WorkerScaffold {
+    private event;
+    private isDev;
+    private fns;
+    private matched;
+    constructor(event: FetchEvent, isDev?: boolean);
+    static errorHandler(_error: Error): void;
+    use(path: string | undefined, handler?: Function | MiddlewareHandlerBundle): void;
+    get(path: string | undefined, handler?: Function | MiddlewareHandlerBundle): void | Function;
+    post(path: string | undefined, handler?: Function | MiddlewareHandlerBundle): void | Function;
+    head(path: string | undefined, handler?: Function | MiddlewareHandlerBundle): void | Function;
+    put(path: string | undefined, handler?: Function | MiddlewareHandlerBundle): void | Function;
+    delete(path: string | undefined, handler?: Function | MiddlewareHandlerBundle): void | Function;
+    options(path: string | undefined, handler?: Function | MiddlewareHandlerBundle): void | Function;
+    run(): Promise<Response>;
+}
 export { redirect, rewrite, faviconByBase64, cors, robotsTxt, basicAuth };
 export default WorkerScaffold;
